faa0402e6cd0709136c46a7c45211a2dd75aaf9a
Louis Augey <louis.augey@orange.fr>
2024-05-13 13:34:24 +0200

    euler, runge-kutta, plot

--- main.py
+++ main.py
@@ -1,45 +1,86 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-"""
+# Parameters
+
+n = 1000
 y0 = 5
-k = 3
-x = np.linspace(0, 3, 10000)
-y = y0*np.exp(-k*x)
+x0 = 0
+t = np.linspace(0, 10, n)
+x = np.linspace(0, 10, n)
+k = 0.3
 
-fig = plt.figure(figsize = (20, 20))
-plt.plot(x, y)
-plt.show()
-"""
+# Functions
 
-def derive(y, t):
-    g = 0
-    return (g-y)/tau
+def f(k, y) :
+    '''
+    input : k (float) : constant
+            y (float) : variable
+    
+    output : -k * y (float) : derivative of y
+    '''
+    return -k * y
 
-def euler(derive, y0, t):
-    h = t[1]-t[0]
-    N = len(t)
-    y = np.zeros(N)
+def f_real(k, y0,x) :
+    '''
+    input : k (float) : constant
+            y0 (float) : initial value
+            x (float) : variable
+
+    output : y0 * np.exp(-k * x) (float) : analytical solution
+    '''
+    return y0 * np.exp(-k * x)
+
+def euler_method(t, f, y0):
+    '''
+    input : t (np.array) : time
+            f (function) : derivative function
+            y0 (float) : initial value
+    output : y (np.array) : numerical solution of the euler method
+    '''
+    n = len(t)
+    y = np.zeros(n)
     y[0] = y0
-    for n in range(0,N-1):
-        y[n+1] = y[n] + h*derive(y[n], t[n])
+    h = 10/n
+    for i in range(n-1):
+        y[i+1] = y[i] + h * f(k, y[i])
     return y
 
-# PARAMETRES
-tau = 1/3
-T = 3
-N = 1000
 
-# RESOLUTION DE L'EQUADIFF
-y0 = 5                 # Condition initiale
-t = np.linspace(0,T,N)    # Tableau du temps
-y = euler(derive, y0, t)  # Integration
+def runge_kutta_method(t, f, y0):
+    '''
+    input : t (np.array) : time
+            f (function) : derivative function
+            y0 (float) : initial value
+    output : y (np.array) : numerical solution of the runge-kutta method
+    '''
+    n = len(t)
+    y = np.zeros(n)
+    x = np.zeros(n)
+    y[0] = y0
+    x[0]=x0
+    h = 10/n
+    for i in range(n-1):
+        k1 = h * f(k, y[i])
+        k2 = h * f(k + h/2, y[i] + k1/2)
+        k3 = h * f(k + h/2, y[i] + k2/2)
+        k4 = h * f(k + h, y[i] + k3)
+        y[i+1] = y[i] + (k1 + 2*k2 + 2*k3 + k4)/6
+        x[i+1] = x[i] + h
+    return y
 
-# COURBES
-plt.plot(t, y, ".", ms =2 ,label='y(t) pour N={}'.format(N))
-plt.legend()
-plt.xlabel('t')
-plt.ylim(0,12)
-plt.ylabel('y')
+# Compute
+analytic_function = f_real(k, y0, x)
+y_euler = euler_method(t, f, y0)
+y_runge_kutta = runge_kutta_method(t, f, y0)
+
+# Plot
+plt.plot(t, analytic_function, label='Analytical')
+plt.plot(t, y_euler, label='Euler')
+plt.plot(t, y_runge_kutta, label='Runge-Kutta')
+plt.title(f"Approximated and exact solutions of the ODE y\' = -{k}*y with y(0) = 5")
 plt.grid()
+plt.xlabel('t')
+plt.ylabel('y(t)')  
+plt.legend()
 plt.show()
